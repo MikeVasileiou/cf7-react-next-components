8cc37835b32c642f0b776eda4b98e57e40f35c4c
Mike.Vasileiou <michvasi@hotmail.com>
2025-06-02 17:17:14 +0300

    feat: added namechanger component

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,46 @@
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 import Layout from "./components/Layout.tsx";
-import Todo from "./components/Todo/Todo.tsx";
-// import CounterWithReducer from "./components/CounterWithReducer.tsx";
-// import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
-// import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
-// import NameChanger from "./components/NameChanger.tsx";
+import NameChanger from "./components/NameChanger.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
-// import CodingFactoryLogo from "./components/CodingFactoryLogo.tsx";
+// import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
+// import CounterAdvanced from "./components/CounterAdvanced.tsx";
+// import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook";
+// import CounterWithReducer from "./components/CounterWithReducer.tsx";
+// import Todo from "./components/Todo/Todo.tsx";
 
 function App() {
 
+    return (
+        <>
+            <Layout>
+                {/*<ClassComponent/>*/}
+                {/*<FunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponent/>*/}
+                {/*<ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component with Props!"/>*/}
+                {/*<ArrowFunctionalComponentWithPropsType*/}
+                {/*  title="Is a Arrow Functional Component with Props!"*/}
+                {/*  description="this is a description"*/}
+                {/*/>*/}
 
-  return (
-    <>
-      <Layout>
-        {/*<ClassComponent/>*/}
-        {/*<FunctionalComponent/>*/}
-        {/*<ArrowFunctionalComponent/>*/}
-        {/*<ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component with Props!" />*/}
-        {/*<ArrowFunctionalComponentWithPropsType*/}
-        {/*    title="Is an Arrow Functional Component with Pros"*/}
-        {/*    description="Functional Component with Props type " />*/}
+                {/*<ClassComponentWithState/>*/}
+                {/*<FunctionalComponentWithState/>*/}
+                {/*<Counter/>*/}
+                <NameChanger/>
+                {/*<CounterWithMoreStates/>*/}
+                {/*<CounterAdvanced/>*/}
+                {/*<CounterWithCustomHook/>*/}
+                {/*<CounterAdvancedWithCustomHook/>*/}
 
-  {/*<h1 className="text-center text-2xl font-bold">This is heading 1 title</h1>*/}
-  {/*<FunctionalComponent/>*/}
-  {/*    <ClassComponentWithState/>*/}
-  {/*      <FunctionalComponentWithState/>*/}
-  {/*      <Counter/>*/}
-  {/*        <NameChanger/>*/}
-  {/*        <CounterWithMoreStates/>*/}
-  {/*        <CounterAdvanced/>*/}
-  {/*        <CounterWithCustomHook/>*/}
-  {/*        <CounterWithReducer/>*/}
-<Todo/>
-      </Layout>
-  </>
-  )
+                {/*<CounterWithReducer/>*/}
+                {/*<Todo/>*/}
+
+            </Layout>
+        </>
+    )
 }
 
 export default App
--- src/components/NameChanger.tsx
+++ src/components/NameChanger.tsx
@@ -1,31 +1,28 @@
-import {useState} from 'react';
-import * as React from "react";
+import { useState, useEffect } from 'react';
 
 const NameChanger = () => {
-    const [name, setName] = useState("")
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        document.title = name ? `Hello, ${name}!` : "Hello, Stranger!";
+    },[name])
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-
-        setName(e.target.value)
+        setName(e.target.value);
     }
+
     return (
         <>
-        <h1 className="text-center text-xl pt-4">Hello, {name || "Stranger"}</h1>
+            <h1 className="text-center text-xl pt-4">Hello, {name || "Stranger"}</h1>
             <div className="text-center mt-4">
-            <input
-                type="text"
-                value={name}
-                onChange={handleChange}
-                className="border px-4 py-2"
-            />
-
-
+                <input
+                    type="text"
+                    value={name}
+                    onChange={handleChange}
+                    className="border px-4 py-2"
+                />
             </div>
         </>
-
-
-
-
     )
 }
 export default NameChanger;
